697283455a11a18ebf2ccf303884e9a12384ebe1
lufranski <lufranski@gmail.com>
2022-11-29 15:59:44 +0100

    Bonus 1

---
+++ bonus/js/main.js
@@ -0,0 +1,49 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            error: false,
+            newTask: '',
+            tasks: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTask(){
+            
+            if(this.newTask.length < 5 || this.newTask === ''){
+                
+                // error
+                this.error = true;
+
+            }else {
+                
+                this.tasks.unshift({text:this.newTask, done:false});
+
+                this.error = false;
+            
+            }
+            
+            this.newTask='';
+
+        },
+        deleteTask(indexArray){
+
+            this.tasks.splice(indexArray, 1);
+
+        }
+    }
+}).mount('#app')
